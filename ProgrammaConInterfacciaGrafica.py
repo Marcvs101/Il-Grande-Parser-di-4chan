a01aba87e7e88c52dd1f93b7f18b8a1411f65b04
Marcvs101 <marcocuoci@yahoo.it>
2018-07-20 23:37:14 +0200

    Interfaccia funzionante
    Un passo in avanti verso la conquista del mondo

--- ProgrammaConInterfacciaGrafica.py
+++ ProgrammaConInterfacciaGrafica.py
@@ -8,6 +8,8 @@ from Logger import Logger
 board_dict = {}
 global_filter_set = set()
 
+targeted_board = "GLOBAL"
+
 finestra = gui("Il Grande Parser di 4Chan","600x300")
 
 # Util
@@ -16,14 +18,20 @@ def add_board_util(board):
     board_dict[board] = {}
     board_dict[board]["Watchdog"] = ChanBoardWatchdog(board)
     board_dict[board]["Filtri"] = set()
-    #board_dict[board]["Watchdog"].start()
-    update_board_gui_main()
-    hide_board_gui_new()
+    logf = Logger("log_"+board+".txt")
+    board_dict[board]["Watchdog"].setLogger(logf)
+    board_dict[board]["Watchdog"].start()
 
 def remove_board_util(board):
     if (board in board_dict):
         board_dict[board]["Watchdog"].stop()
-        board_dict.remove(board)
+        for i in board_dict[board]["Filtri"]:
+            try: finestra.removeLabel("ThreadFilterEntry<"+str(i)+">")
+            except: print("Non trovato "+str(i))
+        for i in board_dict[board]["Watchdog"].getMonitoredSet():
+            try: finestra.removeLabel("ThreadListEntry<"+str(i)+">")
+            except: print("Non trovato "+str(i))
+        board_dict.pop(board,None)
 
 def apply_filter_util(board,filter_set = set()):
     if (board in board_dict):
@@ -47,11 +55,46 @@ def main_add_board_button():
 def new_board_confirm_button():
     board = finestra.getEntry("NuovaBoardEntry")
     if (board == None or board == ""):
+        update_board_gui_main()
+        hide_board_gui_new()
         return
     add_board_util(board)
+    update_board_gui_main()
+    hide_board_gui_new()
 
 def main_add_filter_button():
     print("NOT IMPLEMENTED YET")
+
+def goto_board(var):
+    global targeted_board
+    targeted_board = var
+    show_thread_gui_main(var)
+
+def thread_add_filter_button():
+    show_filter_gui_new()
+
+def new_filter_confirm_button():
+    f = create_filter_util(finestra.getEntry("NuovoFiltroEntry"))
+    if (f == None):
+        hide_filter_gui_new()
+        update_thread_gui_main(targeted_board)
+        return
+    board_dict[targeted_board]["Filtri"].add(f)
+    apply_filter_util(targeted_board,board_dict[targeted_board]["Filtri"])
+    hide_filter_gui_new()
+    update_thread_gui_main(targeted_board)
+
+def delete_board_button():
+    global targeted_board
+    try: finestra.removeButton(str(targeted_board))
+    except: print("Non trovato "+str(targeted_board))
+    for i in board_dict[targeted_board]["Watchdog"].getMonitoredSet():
+        try: finestra.removeLabel("ThreadListEntry<"+str(i)+">")
+        except: print("Non trovato "+str(i))
+    remove_board_util(targeted_board)
+    hide_thread_gui_main()
+    hide_filter_gui_new()
+    targeted_board = "GLOBAL"
 
 # Costruttori
 def init_board_gui_main():
@@ -59,50 +102,43 @@ def init_board_gui_main():
     finestra.setFont(18)
 
     finestra.startScrollPane("MainBoards", row=0, column=0)
-
     finestra.addLabel("MainBoardsTitolo", "Board monitorate")
     finestra.addButton("Aggiungi nuova board",main_add_board_button)
-
     finestra.stopScrollPane()
 
-
     finestra.startScrollPane("MainFiltri", row=1, column=0)
-
     finestra.addLabel("MainFiltriTitolo", "Filtri globali")
-
     finestra.addButton("Aggiungi nuovo filtro globale",main_add_filter_button)
-
     finestra.stopScrollPane()
 
 def init_board_gui_new():
     finestra.startSubWindow("NuovaBoardFinestra",modal=True)
-
     finestra.addLabel("NuovaBoardTitolo","Inserire la board da monitorare, lasciare vuoto per annullare")
     finestra.addEntry("NuovaBoardEntry")
     finestra.addButton("Procedi con la board",new_board_confirm_button)
-    
     finestra.stopSubWindow()
 
 def init_thread_gui_main():
     finestra.startSubWindow("ThreadFinestra",modal=False)
-
+    finestra.addLabel("ThreadTitolo","ERROR")
+    
     finestra.startScrollPane("ThreadFiltri", row=1, column=0)
-
     finestra.addLabel("ThreadFiltriTitolo", "Filtri")
-
-    finestra.addButton("Aggiungi nuovo filtro",main_add_board_button)
-
+    finestra.addButton("Aggiungi nuovo filtro",thread_add_filter_button)
     finestra.stopScrollPane()
 
+    finestra.startScrollPane("ThreadTrovati", row=2, column=0)
+    finestra.addLabel("ThreadTrovatiTitolo", "Trovati")
+    finestra.stopScrollPane()
+
+    finestra.addButton("ThreadDelete",delete_board_button)
     finestra.stopSubWindow()
 
 def init_filter_gui_new():
     finestra.startSubWindow("NuovoFiltroFinestra",modal=True)
-
     finestra.addLabel("NuovoFiltroTitolo","Inserire una o più parole separate da spazi da monitorare, lasciare vuoto per annullare")
     finestra.addEntry("NuovoFiltroEntry")
-    finestra.addButton("Procedi con il filtro",new_board_confirm_button)
-
+    finestra.addButton("Procedi con il filtro",new_filter_confirm_button)
     finestra.stopSubWindow()
 
 # Azionatori
@@ -113,8 +149,7 @@ def show_thread_gui_main(thread):
     update_thread_gui_main(thread)
     finestra.showSubWindow("ThreadFinestra", hide = False)
 
-def show_filter_gui_new(thread):
-    update_filter_gui_new(thread)
+def show_filter_gui_new():
     finestra.showSubWindow("NuovoFiltroFinestra", hide = False)
 
 # Nasconditori
@@ -133,12 +168,41 @@ def hide_filter_gui_new():
 # Aggiornatori
 def update_board_gui_main():
     finestra.openScrollPane("MainBoards")
+    for i in board_dict:
+        try: finestra.removeButton(str(i))
+        except: print("Non trovato "+str(i))
 
+    for i in board_dict:
+        try: finestra.addButton(str(i),goto_board)
+        except: print("Errore generico "+str(i))        
+    finestra.stopScrollPane()
+
+    finestra.openScrollPane("MainFiltri")
+    
     finestra.stopScrollPane()
 
 def update_thread_gui_main(thread):
-    finestra.openScrollPane("Boards")
+    finestra.setLabel("ThreadTitolo",thread)
+    finestra.openScrollPane("ThreadFiltri")
+    for thr in board_dict:
+        for i in board_dict[thr]["Filtri"]:
+            try: finestra.removeLabel("ThreadFilterEntry<"+str(i)+">")
+            except: print("Non trovato "+str(i))
 
+    for i in board_dict[thread]["Filtri"]:
+        try: finestra.addLabel("ThreadFilterEntry<"+str(i)+">",str(i))
+        except: print("Non trovato "+str(i))
+    finestra.stopScrollPane()
+    
+    finestra.openScrollPane("ThreadTrovati")
+    for thr in board_dict:
+        for i in board_dict[thr]["Watchdog"].getMonitoredSet():
+            try: finestra.removeLabel("ThreadListEntry<"+str(i)+">")
+            except: print("Non trovato "+str(i))
+
+    for i in board_dict[thread]["Watchdog"].getMonitoredSet():
+        try: finestra.addLabel("ThreadListEntry<"+str(i)+">",str(i))
+        except: print("Non trovato "+str(i))
     finestra.stopScrollPane()
 
 init_board_gui_main()
